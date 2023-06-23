c04ed5073ad0995cf22ce7af44b9065e95741514
なるみ <toucans-cutouts0f@icloud.com>
2023-06-24 02:16:09 +0800

    refactor: refactor Loan Calculator module in LangTools package
    - Rename `langtools/mortgage.py` to `langtools/loan.py`
    - Modify `langtools/__init__.py` to import `LoanCalculator` from `langtools/loan.py`
    - Remove `langtools/mortgage.py` file
    
    Signed-off-by: なるみ <toucans-cutouts0f@icloud.com>

--- langtools/__init__.py
+++ langtools/__init__.py
@@ -1,4 +1,4 @@
 from .finance.ticker import QueryTicker
-from .mortgage import LoanCalculator
+from .loan import LoanCalculator
 from .visa import ExchangeRateCalculator
 from .web_browser import WebBrowser
