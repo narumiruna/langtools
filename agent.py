c7e66eda4711e98e2b1bd07709ed0ec6ec4022b6
なるみ <toucans-cutouts0f@icloud.com>
2023-06-23 12:43:48 +0800

    feat: add ChatOpenAI agent to project
    - Add a new file "agent.py"
    - Import modules and classes from "langchain" and "langtools"
    - Load environment variables using "dotenv"
    - Initialize a new instance of "ChatOpenAI" using a specific model name
    - Create a list of tools, with "WebBrowser" as the only item
    - Initialize an agent using "initialize_agent" function with specified parameters
    - Run a loop which takes input from user and returns an agent response
    
    Signed-off-by: なるみ <toucans-cutouts0f@icloud.com>

---
+++ agent.py
@@ -0,0 +1,27 @@
+from dotenv import load_dotenv
+from langchain.agents import AgentType, initialize_agent
+from langchain.chat_models import ChatOpenAI
+from langchain.memory import ConversationBufferMemory
+
+from langtools.web_browser import WebBrowser
+
+
+def main():
+    load_dotenv()
+    llm = ChatOpenAI(model_name="gpt-3.5-turbo-0613")
+    tools = [
+        WebBrowser(),
+    ]
+    memory = ConversationBufferMemory(memory_key='chat_history', return_messages=True)
+    agent = initialize_agent(tools=tools, llm=llm, agent=AgentType.OPENAI_FUNCTIONS, memory=memory, verbose=True)
+    while True:
+        try:
+            question = input("User: ")
+            resp = agent.run(question)
+            print('Agent:', resp)
+        except KeyboardInterrupt:
+            break
+
+
+if __name__ == '__main__':
+    main()
