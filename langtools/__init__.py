08e4816c77c7b6ceaba5d99ccdabb3a3b04c71bf
なるみ <toucans-cutouts0f@icloud.com>
2023-06-24 02:11:35 +0800

    chore: remove PornHub references from project tools and dependencies
    - Remove PornHubSearch tool from langtools
    - Remove pornhub-api dependency from pyproject.toml
    
    Signed-off-by: なるみ <toucans-cutouts0f@icloud.com>

--- langtools/__init__.py
+++ langtools/__init__.py
@@ -1,5 +1,4 @@
 from .finance.ticker import QueryTicker
 from .mortgage import LoanCalculator
-from .pornhub import PornHubSearch
 from .visa import ExchangeRateCalculator
 from .web_browser import WebBrowser
